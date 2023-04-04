fec4780fd09675ac5cc37c089b91e9c30a3bdf81
Mohsen Yazdanian <yazdanian.my@gmail.com>
2023-04-04 23:36:46 +0330

    feat(urls): document the project using drf-yasg package

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import re_path, path, include
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from django.contrib import admin
-from django.urls import path
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    info=openapi.Info(
+        title="SE Project",
+        default_version="0.1",
+    ),
+    url="http://127.0.0.1:8000/",
+    public=True,
+    authentication_classes=None,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(
+        route=r"^swagger/$",
+        view=schema_view.with_ui(),
+        name="schema-swagger-ui",
+    ),
+    path(route="admin/", view=admin.site.urls),
 ]
