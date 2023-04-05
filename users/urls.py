3e051111b17495b614d4706375f871204c6cc2fa
Mohsen Yazdanian <yazdanian.my@gmail.com>
2023-04-05 13:44:43 +0330

    feat(urls): add a url pattern in order to generate a jwt token for a user, while signing in

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
+from rest_framework.authtoken import views as authtoken_views
 from users import views as users_views
 from django.urls import path
 
 
 urlpatterns = [
     path(route="sign-up/", view=users_views.UserCreateAPIView.as_view()),
+    path(route="token/", view=authtoken_views.obtain_auth_token),
 ]
