408aa454b05c1682187eace5c83c2404756bdd76
Mohsen Yazdanian <yazdanian.my@gmail.com>
2023-04-10 00:28:41 +0330

    feat(models): add Product model to products app

--- products/models.py
+++ products/models.py
@@ -1,2 +1,37 @@
 from django.db import models
 
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+    )
+    description = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+    type = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+    )
+    brand = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+    )
+    size = models.PositiveSmallIntegerField(
+        blank=False,
+        null=False,
+    )
+    price = models.PositiveIntegerField(
+        blank=False,
+        null=False,
+    )
+    color = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+    )
