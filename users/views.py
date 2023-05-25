51bf0fc8713f5686e4536ec9f3f556052eabd0eb
mohsen yazdanian <yazdanian.my@gmail.com>
2023-05-25 15:08:21 +0330

    feat(views): add a view to get the list of registered users for development purposes

--- users/views.py
+++ users/views.py
@@ -1,8 +1,13 @@
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.generics import CreateAPIView
-from users import serializers
-
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.permissions import AllowAny
+from users import serializers, models
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = serializers.UserRegisterSerializer
+    permission_classes = [AllowAny]
+
+
+class UserListAPIView(ListAPIView):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
     permission_classes = [AllowAny]
